5db067dbfa5bf8affe3098637859814c155b33b8
alexis blas <91743625+golem427@users.noreply.github.com>
2023-01-16 18:30:36 +0100

    nav

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function home(): Response
+    public function homeController(): Response
     {
         return $this->render('home.html.twig') 
         ;

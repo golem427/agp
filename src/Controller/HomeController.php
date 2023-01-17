fdcec10e84a5e7b2ed47349711d621c0d4843fcf
alexis blas <91743625+golem427@users.noreply.github.com>
2023-01-17 15:11:05 +0100

    build

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
+    #[Route('/home', name: 'home', methods: ['GET'])]
     public function homeController(): Response
     {
         return $this->render('home.html.twig') 

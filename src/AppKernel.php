7d4856011aeba84da8a71e19d6753d9e776fede8
alexis blas <91743625+golem427@users.noreply.github.com>
2023-01-19 17:24:33 +0100

    errorfixtures

--- src/AppKernel.php
+++
@@ -1,8 +0,0 @@
-<!-- // app/AppKernel.php
-
-// ...
-// registerBundles() -->
-if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
-    // ...
-    $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-}

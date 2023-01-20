05141821c7f7b9664d2bf0eae5f4f7222e446d2d
alexis blas <91743625+golem427@users.noreply.github.com>
2023-01-20 15:07:15 +0100

    fake-user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,51 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
+use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+    private $hasher;
 
-        $manager->flush();
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+            $this->hasher = $hasher;
     }
+
+
+    public function load(ObjectManager $manager): void
+{
+      // use the factory to create a Faker\Generator instance
+$faker = Factory::create();
+
+// generate data by calling methods
+// echo $faker->name();
+// 'Vince Sporer'
+// echo $faker->email();
+// 'walter.sophia@hotmail.com'
+// echo $faker->text();
+// 'Numquam ut mollitia at consequuntur inventore dolorem.'
+
+// On crée un user
+
+for ($i=0; $i<6; $i++){
+    $user = new User();
+
+           $user->setEmail('user@test.fr')
+                ->setPrenom($faker->firstName())
+                ->setNom($faker->lastName())
+                ->setTelephone(($faker->phoneNumber()))
+                ->setApropos($faker->text())
+                ->setInstagram('instagram');
+
+            $password=$this->hasher->hashPassword($user, 'password');
+            $user->setPassword($password);
+
+            $manager->persist($user);}
+            $manager->flush();
+}
 }

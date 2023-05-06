bc35a2b073d996e5f3dffdfeb43c8cd53afab2a8
alexis blas <91743625+golem427@users.noreply.github.com>
2023-05-06 16:06:05 +0200

    sitemapController

---
+++ src/Controller/SitemapController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpKernel\UriSigner;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SitemapController extends AbstractController
+{
+    #[Route('/sitemap', name: 'sitemap', defaults: ['_format' => 'xml'])]
+    public function sitemap(Request $request, UriSigner $uriSigner): Response
+    {   
+        $uriSigner->checkRequest($request);
+        return $this->render('sitemap/sitemap.xml.twig', [
+            'controller_name' => 'SitemapController',
+        ]);
+    }
+}

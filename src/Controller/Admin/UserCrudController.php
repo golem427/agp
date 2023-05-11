9f42d1b2482fab733ffcfc9a20ffe2795cafac59
alexis blas <91743625+golem427@users.noreply.github.com>
2023-05-11 14:39:12 +0200

    changepw-crudUser-success

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,10 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -20,10 +22,18 @@ class UserCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('email'),
             TextField::new('nom'),
             TextField::new('password'),
+            Field::new('plainPassword', 'New password')
+            ->onlyOnForms()
+            ->setFormType(RepeatedType::class)
+            ->setFormTypeOptions([
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'first_options' => ['hash_property_path' => 'password', 'label' => 'Nouveau mot de passe'],
+                'second_options' => ['label' => 'Répéter le mot de passe'],
+            ]),
         ];
     }
     public function configureActions(Actions $actions): Actions
@@ -35,6 +45,6 @@ class UserCrudController extends AbstractCrudController
     {
         return $crud
                     ->setPageTitle('index', 'Administrateur')
-                    ->setPageTitle('edit', 'Editer les utilisateurs');
+                    ->setPageTitle('edit', 'Changer mon mot de passe');
     }
 }
